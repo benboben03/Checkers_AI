6b0bee747ec0139435a4ef5db7847a85a4609695
sebseb100 <seisaian@uci.edu>
2023-11-12 18:32:55 -0800

    Good

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -39,7 +39,7 @@ class StudentAI():
         #If the opponent has made a move, it is executed on the internal game board.
         else:
             self.color = 1
-            self.monte_carlo_tree.root_node = TreeNode(self.board, self.color, None, None)
+            self.monte_carlo_tree.root_node = TreeNode(self.board, self.color , None, None)
 
             possible_moves = self.board.get_all_possible_moves(self.color)
             f = possible_moves[0][1]
@@ -69,91 +69,17 @@ class StudentAI():
         return move_chosen
     
     def play_move(self, move, color):
-        """
-        Updates board and tree root using Move given,
-        either Move we just played or Move given by opponent.
-        """
         self.board.make_move(move, color)
-        
-        for child in self.monte_carlo_tree.root_node.children.items():
-            if str(move) == str(child[0]) and child[1] is not None:
-                self.monte_carlo_tree.root_node = child[1]
+
+        for child_move, child_node in self.monte_carlo_tree.root_node.children.items():
+            if child_node is not None and str(child_move) == str(move):
+                self.monte_carlo_tree.root_node = child_node
                 self.monte_carlo_tree.root_node.parent = None
                 return
 
+        # If the move is not in the existing children, create a new TreeNode
         self.monte_carlo_tree.root_node = TreeNode(self.board, {1: 2, 2: 1}[color], None, None)
-    
-class MonteCarloTreeSearch():
-    '''
-    State Representation: Represent the current state of the checkers game. This includes the positions of pieces on the board, the current player's turn, and any additional relevant information.
-   
-    Selection and Expansion: Use MCTS to traverse the tree, selecting nodes based on UCB values. At each iteration, expand the tree by adding child nodes corresponding to possible moves in the game.
-   
-    Simulation: Perform simulations (rollouts) from the newly added nodes by making random moves until a terminal state (win, lose, or draw) is reached.
-   
-    Backpropagation: Update the statistics of nodes along the path from the leaf node to the root based on the outcome of the simulation.
-   
-    Decision: After a certain number of iterations or a specified time limit, select the move that corresponds to the child node with the highest visit count.
-    '''
-    def __init__(self, root):
-        self.root_node = root
-          
-    def tree_search(self, time_limit) -> Move:
-        '''
-        Performs Monte Carlo Tree Search until time runs out.
-        Returns the best move.
-        '''
-        timeout = time() + time_limit
-                
-        while time() < timeout:
-            node = self.selection(self.root_node)
-            
-            temp_board = deepcopy(node.board)
-            temp_color = node.color
-            win_val = temp_board.is_win({1: 2, 2: 1}[temp_color])
-            
-            while not win_val:
-                possible_moves = temp_board.get_all_possible_moves(temp_color)
-                i = random.randint(0, len(possible_moves) - 1)
-                j = random.randint(0, len(possible_moves[i]) - 1)
-                temp_move = possible_moves[i][j]
-                
-                temp_board.make_move(temp_move, temp_color)
-                win_val = temp_board.is_win(temp_color)
-                temp_color = {1: 2, 2: 1}[temp_color]
-    
-            if win_val == {1: 2, 2: 1}[node.color]:
-                win_for_parent = 1
-            elif win_val == node.color:
-                win_for_parent = -1
-            elif win_val == -1:
-                win_for_parent = 0
-                
-            node.backpropogation(win_for_parent)
-
-        return self.the_chosen_one()
-    
-    def selection(self, node) -> 'TreeNode':
-        '''
-        Recursively traverses the tree to find a terminal node with the highest UCB value,
-        then expands a new unexplored node.
-        '''
-        if len(node.children) == 0:
-            return node
-        if None not in node.children.values():
-            sorted_children = sorted(node.children.values(), key=lambda x: x.upper_confidence_bound, reverse=True)
-            return self.selection(sorted_children[0])
-        for move, child in node.children.items():
-            if child is None:
-                node.children[move] = TreeNode(node.board, {1: 2, 2: 1}[node.color], move, node)
-                return node.children[move]
-    
-    def the_chosen_one(self) -> Move:
-        '''Node with the highest visit count is selected'''
-        best_child = max(self.root_node.children.items(), key=lambda x: x[1].current_node_visit_counter)
-        return best_child[0]
-
-    
+        
 class TreeNode():
     def __init__(self, board, color, move, parent):
         self.board = deepcopy(board)
@@ -163,7 +89,6 @@ class TreeNode():
         self.parent_win_counter = 0
         self.current_node_visit_counter = 1
       
-        
         #First move
         if move is not None:
             self.board.make_move(move, {1: 2, 2: 1}[self.color])
@@ -200,3 +125,76 @@ class TreeNode():
             exploration_term = sqrt(2) * sqrt(log(self.parent.current_node_visit_counter) / self.current_node_visit_counter)
             winning_ratio = self.parent_win_counter / self.current_node_visit_counter
             self.upper_confidence_bound = winning_ratio + exploration_term
+    
+class MonteCarloTreeSearch():
+    '''
+    State Representation: Represent the current state of the checkers game. This includes the positions of pieces on the board, the current player's turn, and any additional relevant information.
+   
+    Selection and Expansion: Use MCTS to traverse the tree, selecting nodes based on UCB values. At each iteration, expand the tree by adding child nodes corresponding to possible moves in the game.
+   
+    Simulation: Perform simulations (rollouts) from the newly added nodes by making random moves until a terminal state (win, lose, or draw) is reached.
+   
+    Backpropagation: Update the statistics of nodes along the path from the leaf node to the root based on the outcome of the simulation.
+   
+    Decision: After a certain number of iterations or a specified time limit, select the move that corresponds to the child node with the highest visit count.
+    '''
+    def __init__(self, root):
+        self.root_node = root
+          
+    def tree_search(self, time_limit) -> Move:
+        '''
+        Performs Monte Carlo Tree Search until time runs out.
+        Returns the best move.
+        '''
+        break_time = time() + time_limit
+                
+        while time() < break_time:
+            node = self.selection(self.root_node)
+            
+            temp_board = deepcopy(node.board)
+            temp_color = node.color
+            win_val = temp_board.is_win({1: 2, 2: 1}[temp_color])
+            
+            while not win_val:
+                possible_moves = temp_board.get_all_possible_moves(temp_color)
+                i = random.randint(0, len(possible_moves) - 1)
+                j = random.randint(0, len(possible_moves[i]) - 1)
+                temp_move = possible_moves[i][j]
+                
+                temp_board.make_move(temp_move, temp_color)
+                win_val = temp_board.is_win(temp_color)
+                temp_color = {1: 2, 2: 1}[temp_color]
+    
+            if win_val == {1: 2, 2: 1}[node.color]:
+                win_for_parent = 1
+            elif win_val == -1:
+                win_for_parent = 0
+            elif win_val == node.color:
+                win_for_parent = -1
+                
+            node.backpropogation(win_for_parent)
+            
+        return self.the_chosen_one()
+
+     def the_chosen_one(self) -> Move:
+        '''
+        Node with the highest visit count is selected
+        '''
+        best_child = max(self.root_node.children.items(), key=lambda x: x[1].current_node_visit_counter)
+        return best_child[0]
+    
+    def selection(self, node) -> 'TreeNode':
+        '''
+        Recursively traverses the Monte Carlo Tree Search (MCTS) tree to locate a terminal node with the highest 
+        Upper Confidence Bound (UCB) value. Subsequently, it expands a new unexplored node for further exploration 
+        and exploitation in the search space.
+        '''
+        if len(node.children) == 0:
+            return node
+        if None not in node.children.values():
+            sorted_children = sorted(node.children.values(), key=lambda x: x.upper_confidence_bound, reverse=True)
+            return self.selection(sorted_children[0])
+        for move, child in node.children.items():
+            if child is None:
+                node.children[move] = TreeNode(node.board, {1: 2, 2: 1}[node.color], move, node)
+                return node.children[move]
